f9848712fb5f71a64e065304d245ff39687be071
Rupesh115 <rupeshrajbhar115@gmail.com>
2024-12-09 08:52:13 +0530

    images changes

--- pages/about.js
+++ pages/about.js
@@ -18,7 +18,7 @@ import BreadCrumb from "../src/components/Breadcrumb";
 import styles from "@/styles/pages/about.module.scss";
 
 // IMAGES //
-import AboutPage from "../public/img/about/about_page.jpeg";
+import AboutPage from "../public/img/about/about_page.png";
 
 // DATA //
 
